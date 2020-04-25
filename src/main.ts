3998ee5755d71418ea6de379d1426baf2d0bded3
kayac <egg734631@gmail.com>
2020-04-25 23:02:21 +0800

    Remove Comment

--- src/main.ts
+++ src/main.ts
@@ -22,42 +22,42 @@ async function main(env: NodeJS.Dict<string>) {
     })
   ]);
 
-  // const models = {
-  //   users: User,
-  //   orders: Order
-  // };
+  const models = {
+    users: User,
+    orders: Order
+  };
 
-  // for (const [tablename, Model] of Object.entries(models)) {
-  //   //
-  //   Redis.observe(tablename, async res => {
-  //     //
-  //     const data = JSON.parse(res);
+  for (const [tablename, Model] of Object.entries(models)) {
+    //
+    Redis.observe(tablename, async res => {
+      //
+      const data = JSON.parse(res);
 
-  //     console.log(`=== Get data from Redis.${tablename} ===`);
-  //     console.table(data);
+      console.log(`=== Get data from Redis.${tablename} ===`);
+      console.table(data);
 
-  //     console.log(`=== Check if data existed in Postgres.${tablename} ===`);
+      console.log(`=== Check if data existed in Postgres.${tablename} ===`);
 
-  //     const existed = await PG.table(tablename).exist({
-  //       [Model.pk]: data[Model.pk]
-  //     });
+      const existed = await PG.table(tablename).exist({
+        [Model.pk]: data[Model.pk]
+      });
 
-  //     if (existed) {
-  //       //
-  //       console.log(`=== Update Postgres.${tablename} ===`);
+      if (existed) {
+        //
+        console.log(`=== Update Postgres.${tablename} ===`);
 
-  //       await PG.table(tablename)
-  //         //
-  //         .update(Model(data), `${Model.pk} = '${data[Model.pk]}'`);
+        await PG.table(tablename)
+          //
+          .update(Model(data), `${Model.pk} = '${data[Model.pk]}'`);
 
-  //       return;
-  //     }
+        return;
+      }
 
-  //     console.log(`=== Insert into Postgres.${tablename} ===`);
+      console.log(`=== Insert into Postgres.${tablename} ===`);
 
-  //     await PG.table(tablename).insert(Model(data));
-  //   });
-  // }
+      await PG.table(tablename).insert(Model(data));
+    });
+  }
 }
 
 config();
